9ab09f9934151c38ed6f551361c5ec0d8a1c61e0
Antonio Costa <antonio.costa@keeponbudget.com>
2024-12-03 14:56:34 +0000

    DAY4

---
+++ src/AdventOfCode.Day/Day4.cs
@@ -0,0 +1,32 @@
+﻿using AdventOfCode.Common;
+
+namespace AdventOfCode.Day
+{
+    public class Day4 : AdventCalendarDay
+    {
+        protected override int Day => 4;
+        public override void Run(bool isTestMode)
+        {
+            base.Run(isTestMode);
+            var input = GetInput();
+            SolvePuzzleOne(input);
+            SolvePuzzleTwo(input);
+        }
+        private void SolvePuzzleTwo(Day4Input input)
+        {
+            
+        }
+        
+        private void SolvePuzzleOne(Day4Input input)
+        {
+            
+        }
+
+        protected override Day4Input GetInput()
+        {
+            var fileName = GetInputFileName(1);
+            var input1 = File.ReadAllText(fileName);
+            return new Day4Input(input1);
+        }
+    }
+}
---
+++ src/AdventOfCode.Day/Day4Input.cs
@@ -0,0 +1,12 @@
+﻿using AdventOfCode.Common;
+
+namespace AdventOfCode.Day
+{
+    public class Day4Input : IDayInput
+    {
+        public Day4Input(string input)
+        {
+            //TBD
+        }
+    }
+}
