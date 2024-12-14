97d423a77cc92f26caa20fb8e500370b71672dd0
Antonio Costa <antonio.costa@keeponbudget.com>
2024-12-14 22:13:55 +0000

    DAY 14 - WORKING. output Filename contains answer to the second puzzle

---
+++ src/AdventOfCode.Day/Day14.cs
@@ -0,0 +1,114 @@
+﻿using AdventOfCode.Common;
+
+namespace AdventOfCode.Day
+{
+    public class Day14 : AdventCalendarDay
+    {
+        protected override int Day => 14;
+
+        private string _inputCache;
+
+        override public void Run(bool isTestMode)
+        {
+            base.Run(isTestMode);
+            SolvePuzzleOne();
+            SolvePuzzleTwo();
+        }
+
+        private void SolvePuzzleOne()
+        {
+            var input = GetInput();
+
+            Thread.Sleep(1000);
+            Console.Clear();
+
+            Console.WriteLine("Initial Position");
+            input.PrintMap();
+
+            for (int i = 0; i < 100; i++)
+            {
+                if(IsTestMode)
+                    Console.WriteLine($"After {i + 1} second{(i == 0 ? "" : "s")}");
+
+                foreach (var robot in input.Robots)
+                {
+                    robot.Move();
+                }
+
+                if(IsTestMode)
+                    input.PrintMap();
+            }
+
+            Console.WriteLine("Final map");
+            input.PrintMap(true);
+
+            int[] quadrants = new int[4];
+
+            foreach (var robot in input.Robots)
+            {
+                var quadrant = robot.GetQuadrant();
+                if(quadrant >= 0)
+                {
+                    quadrants[quadrant]++;
+                }
+            }
+
+            Console.WriteLine($"Quadrants: {string.Join(", ", quadrants)}");
+            Console.WriteLine($"Quadrants product: {quadrants[0] * quadrants[1] * quadrants[2] * quadrants[3]}");
+        }
+
+        private async void SolvePuzzleTwo()
+        {
+            var input = GetInput();
+            for (int i = 0; i < 10000; i++)
+            {
+                foreach (var robot in input.Robots)
+                {
+                    robot.Move();
+                }
+                var message = "                                ";
+
+                if (ThereIsRobotsInARowOf(10, input.Robots))
+                    message = input.PrintMapImage(i+1);
+
+                ((long)i).PrintProgressBar(10000 - 1, message: $"[{message}]");
+            }
+        }
+
+        private bool ThereIsRobotsInARowOf(int countInRow, IEnumerable<Robot> robots)
+        {
+            foreach (var robot in robots)
+            {
+                var count = 0;
+                var currentX = robot.CurrentPosition.X;
+
+                var otherRobotsInLine = robots.Where(r => r.CurrentPosition.X > currentX && r.CurrentPosition.Y == robot.CurrentPosition.Y).OrderBy(r => r.CurrentPosition.X);
+
+                foreach (var otherRobot in otherRobotsInLine)
+                {
+                    if(otherRobot.CurrentPosition.X == currentX + 1 && robot.CurrentPosition.Y == otherRobot.CurrentPosition.Y)
+                    {
+                        count++;
+                        currentX++;
+
+                        if (count >= countInRow)
+                            return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        protected override Day14Input GetInput()
+        {
+            if (_inputCache == null)
+            {
+                var fileName = GetInputFileName(1);
+                _inputCache = File.ReadAllText(fileName);
+            }
+
+            return new Day14Input(_inputCache, IsTestMode);
+        }
+    }
+}
---
+++ src/AdventOfCode.Day/Day14Input.cs
@@ -0,0 +1,147 @@
+﻿using AdventOfCode.Common;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace AdventOfCode.Day
+{
+    public class Day14Input : IDayInput
+    {
+        public IEnumerable<Robot> Robots { get; set; }
+
+        const int realWidth = 101;
+        const int realHeight = 103;
+        const int testWidth = 11;
+        const int testHeight = 7;
+
+        public bool IsTestRun { get; }
+
+        public Day14Input(string input, bool isTestRun)
+        {
+            IsTestRun = isTestRun;
+            var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            var robots = new List<Robot>();
+            foreach (var line in lines)
+            {
+                var parts = line.Split(' ');
+                var positionPart = parts[0].Split("=")[1];
+                var movementPart = parts[1].Split("=")[1];
+
+                var initialPosition = new Point(
+                    int.Parse(positionPart.Split(',')[0]),
+                    int.Parse(positionPart.Split(',')[1]));
+
+                var movement = new Size(int.Parse(movementPart.Split(',')[0]), int.Parse(movementPart.Split(',')[1]));
+
+                var robot = new Robot(initialPosition, movement, isTestRun ? testWidth : realWidth, isTestRun ? testHeight : realHeight);
+                robots.Add(robot);
+            }
+
+            Robots = robots;
+        }
+
+        public void PrintMap(bool removeMiddle = false)
+        {
+            for (int y = 0; y < (IsTestRun ? testHeight : realHeight); y++)
+            {
+                for (int x = 0; x < (IsTestRun ? testWidth : realWidth); x++)
+                {
+                    if(removeMiddle && (x == Robots.First().MapXMiddle || y == Robots.First().MapYMiddle))
+                    {
+                        Console.Write(" ");
+                        continue;
+                    }
+
+                    var countRobotsInPosition = Robots.Count(r => r.CurrentPosition == new Point(x, y));
+                    Console.Write(countRobotsInPosition == 0 ? "." : countRobotsInPosition.ToString());
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public string PrintMapImage(int second)
+        {
+            int width = IsTestRun ? testWidth : realWidth;
+            int height = IsTestRun ? testHeight : realHeight;
+
+            if (!Directory.Exists("Output"))
+                Directory.CreateDirectory("Output");
+
+            var filePath = $"Output/Map{second:D5}.jpg";
+
+            using (var bitmap = new Bitmap(width * 10, height * 10))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.Clear(Color.White);
+
+                    for (int y = 0; y < height; y++)
+                    {
+                        for (int x = 0; x < width; x++)
+                        {
+                            var countRobotsInPosition = Robots.Count(r => r.CurrentPosition == new Point(x, y));
+                            string text = countRobotsInPosition == 0 ? "." : "●";
+                            g.DrawString(text, SystemFonts.DefaultFont, Brushes.Black, x * 10, y * 10);
+                        }
+                    }
+                }
+
+                bitmap.Save(filePath, ImageFormat.Jpeg);
+            }
+
+            return $"Map saved to {filePath}";
+        }
+
+
+    }
+
+    public class Robot {
+        public Point InitialPosition { get; }
+        public Point CurrentPosition { get; private set; }
+        public Size Movement { get; }
+        public int MapXSize { get; }
+        public int MapYSize { get; }
+        public int MapXMiddle { get; }
+        public int MapYMiddle { get; }
+
+        public Robot(Point initialPosition, Size movement, int x, int y)
+        {
+            InitialPosition = initialPosition;
+            Movement = movement;
+            CurrentPosition = initialPosition;
+            MapXSize = x;
+            MapYSize = y;
+            MapXMiddle = x / 2;
+            MapYMiddle = y / 2;
+        }
+
+        public void Move()
+        {
+            CurrentPosition = Point.Add(CurrentPosition, Movement);
+            if (CurrentPosition.X < 0)
+                CurrentPosition = Point.Add(CurrentPosition, new Size(MapXSize, 0));
+            if (CurrentPosition.Y < 0)
+                CurrentPosition = Point.Add(CurrentPosition, new Size(0, MapYSize));
+            if (CurrentPosition.X >= MapXSize)
+                CurrentPosition = Point.Subtract(CurrentPosition, new Size(MapXSize, 0));
+            if (CurrentPosition.Y >= MapYSize)
+                CurrentPosition = Point.Subtract(CurrentPosition, new Size(0, MapYSize));
+        }
+
+        public int GetQuadrant()
+        {
+            if (CurrentPosition.X < MapXMiddle && CurrentPosition.Y < MapYMiddle)
+                return 0;
+            if (CurrentPosition.X > MapXMiddle && CurrentPosition.Y < MapYMiddle)
+                return 1;
+            if (CurrentPosition.X < MapXMiddle && CurrentPosition.Y > MapYMiddle)
+                return 2;
+            if(CurrentPosition.X == MapXMiddle || CurrentPosition.Y == MapYMiddle)
+                return -1; //to ignore
+            return 3;
+        }
+    }
+
+
+}
